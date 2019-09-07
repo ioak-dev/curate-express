a6a73dbf0a4c0ede94c4bd72ceb45f40aed3b746
Amarendra Sahoo <amarendrait@gmail.com>
2019-09-07 11:09:26 +0530

    Atom-002 : Added note model and note service

---
+++ model/Note.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const Note = new Schema({
+    userId: { type: String },
+    title: { type: String },
+    content: { type: String },
+    tags: { type: String },
+    createdAt: { type: Date },
+    lastModifiedAt: { type: Date }
+});
+
+module.exports = mongoose.model('note', Note);
